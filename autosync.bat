4d9611005b9d5ab7377d36b9cd501dcca39f6f5a
道丘 <daoxsj@163.com>
2021-11-30 15:27:13 +0800

    2021/11/30,15:27:13

--- autosync.bat
+++ autosync.bat
@@ -1,20 +1,20 @@
 @echo off
 
-:: 获取当前脚本的路径
+:: get the path
 cd /d %~dp0
-:: 自动提交
+:: auto submit
 git add . 
  git commit -m "%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push origin master
-@echo 已经完成,
+@echo Done,
 
-SET daoTime=60
+SET daoTime=5
 :dao
 set /a daoTime=daoTime-1
 ping -n 2 -w 500 127.1>nul
 cls
-echo 上传Git完成 ,倒计时退出: %daoTime%秒
+echo Done ,exit soon: %daoTime%seconds
 if %daoTime%==0 (exit) else (goto dao)
 
 
